27a5eeae2c0105ecb84240ef8cae32280c0cda2f
MoiKeyboard <43774527+MoiKeyboard@users.noreply.github.com>
2018-10-02 23:53:19 +0800

    Add files via upload

---
+++ quiz2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int rAge (int studRank);
+int main ()
+{
+    int studRank;
+    printf ("Enter student rank: \n");
+    scanf ("%d", &studRank);
+    printf ("rAge(): %d\n", rAge (studRank));
+    return 0;
+}
+
+int rAge (int studRank)
+{
+    /* Write your code here */
+    static int inc =2;
+    if(studRank==1)
+    {
+        return 8+inc;
+    }
+    if(studRank!=1)
+    {
+        inc +=2;
+        rAge(studRank-1);
+    }
+}
