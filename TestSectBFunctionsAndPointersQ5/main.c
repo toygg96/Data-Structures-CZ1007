5cbe65c12dab593a55bd5f2d80ce47188ef13738
MoiKeyboard <43774527+MoiKeyboard@users.noreply.github.com>
2018-10-02 16:35:46 +0800

    Update main.c
    too many extra stuff

--- TestSectBFunctionsAndPointersQ5/main.c
+++ TestSectBFunctionsAndPointersQ5/main.c
@@ -13,22 +13,21 @@ int main()
 }
 int perfectProd1(int num)
 {
-/* Write your code here */
+    /* Write your code here */
     int n = 1, result = 0,perfectProdNum = 1;
     num-=1;
-    while(num!=0){
-        while (num != n){
-            if (num % n == 0){
+    while(num!=0)
+    {
+        while (num != n)
+        {
+            if (num % n == 0)
                 result+=n;
-                n++;
-            } else {
-                n++;
-            }
+            n++;
+
         }
-        //printf("Num: %d and result: %d\n",num,result);
-        if (result == num) {
+        //printf("\nNum: %d and result: %d\n",num,result);
+        if (result == num)
             perfectProdNum *= result;
-        }
         num--;
         result = 0;
         n = 1;
@@ -37,23 +36,21 @@ int perfectProd1(int num)
 }
 void perfectProd2(int num, int *prod)
 {
-/* Write your code here */
+    /* Write your code here */
     int n = 1, result = 0;
     num-=1;
     *prod = 1;
-    while(num!=0){
-        while (num != n){
-            if (num % n == 0){
+    while(num!=0)
+    {
+        while (num != n)
+        {
+            if (num % n == 0)
                 result+=n;
-                n++;
-            } else {
-                n++;
-            }
+            n++;
         }
         //printf("Num: %d and result: %d\n",num,result);
-        if (result == num) {
+        if (result == num)
             *prod *= result;
-        }
         num--;
         result = 0;
         n = 1;
