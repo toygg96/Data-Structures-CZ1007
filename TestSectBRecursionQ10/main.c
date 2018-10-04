74724fd8ae5fd0c9e7ee05d762799f966a334680
toygg96 <josephkingyiu19@gmail.com>
2018-10-04 23:22:47 +0800

    Add files via upload

---
+++ TestSectBRecursionQ10/main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void rReverseAr (int ar[], int size);
+int main ()
+{
+    int array[80];
+    int size, i;
+    printf ("Enter size: \n");
+    scanf ("%d", &size);
+    printf ("Enter %d numbers: \n", size);
+    for (i = 0; i < size; i++)
+        scanf ("%d", &array[i]);
+    printf ("rReverseAr(): ");
+    rReverseAr (array, size);
+    for (i = 0; i < size; i++)
+        printf ("%d ", array[i]);
+    printf ("\n");
+    return 0;
+}
+
+void rReverseAr (int ar[], int size)
+{
+    /* Write your program code here */
+    int start = 0, end = size - 1, temp;
+    if (start < end)
+    {
+        temp = ar[start];
+        ar[start] = ar[end];
+        ar[end] = temp;
+
+        rReverseAr(ar+1,size-2);
+    }
+}
---
+++ TestSectBRecursionQ3/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+float rPower1 (float num, int p);
+int main ()
+{
+    int power;
+    float number;
+    printf ("Enter the number and power: \n");
+    scanf ("%f %d", &number, &power);
+    printf ("rPower1(): %.2f\n", rPower1 (number, power));
+    return 0;
+}
+
+float rPower1 (float num, int p)
+{
+    /* Write your code here */
+    printf("num-%f p=%d\n",num,p);
+    if(p==0)
+        return 1;
+    else if(p > 0)
+        return num * rPower1(num,p-1); //positive
+    else
+        return 1 / rPower1(num,-p); //negative -> call positive to get negative exponent
+
+}
