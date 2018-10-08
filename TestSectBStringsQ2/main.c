15614bea55d76dec82937c323b663ffb6ebc0c76
toygg96 <josephkingyiu19@gmail.com>
2018-10-08 15:50:34 +0800

    Add files via upload

--- TestSectBStringsQ2/main.c
+++ TestSectBStringsQ2/main.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
-int compareStr(char *s, char *t);
-int main()
-{
-    char a[80],b[80];
-    printf("Enter the first string: \n");
-    gets(a);
-    printf("Enter the second string: \n");
-    gets(b);
-    printf("compareStr(): %d\n", compareStr(a,b));
+int rAge(int studRank);
+int main() {
+    int studRank;
+    printf("Enter student rank: \n");
+    scanf("%d",&studRank);
+    printf("rAge(): %d\n", rAge(studRank));
     return 0;
 }
-int compareStr(char *s, char *t)
-{
-/* Write your code here */
-    int i = 0, diff = 0; // init i and diff to be 0
-    while (s[i] != '\0' || t[i] != '\0'){   // keep comparing if the character is not a terminator string (\0)
-        if((s[i] < t[i]) || (s[i] > t[i])) {   // if s < t or s>t
-                diff = s[i] - t[i];
-                break;
-            }
-            i++;    // increment i
-    }
-    return diff;
+int rAge(int studRank) {
+    /* Write your code here */
+    if(studRank == 1) return 9+studRank; else return 2+rAge(studRank-1);
 }
