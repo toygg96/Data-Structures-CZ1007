5e33f2fc2dbcfbe87172f4c428f52f7c8cc09e47
toygg96 <josephkingyiu19@gmail.com>
2018-10-09 12:27:48 +0800

    Add files via upload

---
+++ TestSectBRecursionQ2/altSolution.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int rAge(int studRank);
+int main() {
+    int studRank;
+    printf("Enter student rank: \n");
+    scanf("%d",&studRank);
+    printf("rAge(): %d\n", rAge(studRank));
+    return 0;
+}
+int rAge(int studRank) {
+    /* Write your code here */
+    if(studRank == 1) return 9+studRank; else return 2+rAge(studRank-1);
+}
