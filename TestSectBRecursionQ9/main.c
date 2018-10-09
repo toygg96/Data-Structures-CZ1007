1b292e8c9b245d0a9cd8b167eb0558b0891523e9
toygg96 <josephkingyiu19@gmail.com>
2018-10-09 12:31:26 +0800

    Add files via upload

--- TestSectBRecursionQ9/main.c
+++ TestSectBRecursionQ9/main.c
@@ -1,31 +1,22 @@
-#include<stdio.h>
-void rFindMaxAr (int *ar, int size, int *max);
-int main ()
-{
-    int ar[50], i, max = 0, size;
-    printf ("Enter array size: \n");
-    scanf ("%d", &size);
-    printf ("Enter %d numbers: \n", size);
-    for (i = 0; i < size; i++)
-        scanf ("%d", &ar[i]);
-    rFindMaxAr (ar, size, &max);
-    printf ("rFindMaxAr(): %d\n", max);
+#include <stdio.h>
+void rFindMaxAr(int *ar, int size, int *max);
+int main() {
+    int ar[50],i,max=0,size;
+    printf("Enter array size: \n");
+    scanf("%d", &size);
+    printf("Enter %d numbers: \n", size);
+    for (i=0; i < size; i++)
+        scanf("%d", &ar[i]);
+    rFindMaxAr(ar,size,&max);
+    printf("rFindMaxAr(): %d\n", max);
     return 0;
 }
-
-void rFindMaxAr (int *ar, int size, int *max)
-{
+void rFindMaxAr(int *ar, int size, int *max) {
     /* Write your code here */
-    printf("%d %d\n",*(ar+size-1),*max);
-    if(size==0)
-    {
-        *max=*ar;
+    if(size == 0) return;
+    else if (*ar > *max) {
+            *max = *ar;
+            rFindMaxAr(++ar,size-1,max);
     }
-    else if(size>=1)
-    {
-        if(*max<*(ar+size-1))
-            *max=*(ar+size-1);
-        rFindMaxAr(ar,--size,max);
-    }
-
+    else rFindMaxAr(++ar,size-1,max);
 }
