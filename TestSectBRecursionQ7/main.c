e3f7a8bf5d9849c380a750751767665633303fe8
toygg96 <josephkingyiu19@gmail.com>
2018-10-09 12:30:27 +0800

    Add files via upload

--- TestSectBRecursionQ7/main.c
+++ TestSectBRecursionQ7/main.c
@@ -1,23 +1,17 @@
-#include<stdio.h>
-int rStrLen (char *s);
-int main ()
-{
+#include <stdio.h>
+int rStrLen(char *s);
+int main() {
     char str[80];
-    printf ("Enter the string: \n");
-    gets (str);
-    printf ("rStrLen(): %d\n", rStrLen (str));
+    printf("Enter the string: \n");
+    gets(str);
+    printf("rStrLen(): %d\n", rStrLen(str));
     return 0;
 }
-
-int rStrLen (char *s)
-{
-    /* Write your program code here */
-    printf("%s\n",s);
-    if(*s==NULL)
-    {
-        return 0;
-    }
-    if(*s==' ')
-        return rStrLen(++s);
-    return 1 + rStrLen(++s);
+int rStrLen(char *s) {
+   static int c = 0;
+   while (s[c] != '\0') {
+      c++;
+      rStrLen(s);
+   }
+   return c;
 }
