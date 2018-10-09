999bb2adfb1fe13fc88d40ccd54bfa6c44615a71
MoiKeyboard <43774527+MoiKeyboard@users.noreply.github.com>
2018-10-09 16:33:44 +0800

    Update main.c

--- TestSectBStringsQ10/main.c
+++ TestSectBStringsQ10/main.c
@@ -13,25 +13,20 @@ int main()
 int countSubstring(char str[], char substr[])
 {
 /* Write your program code here */
-    int i, j, l1 = 0, l2 = 0, count = 0, count1 = 0;
-    while(str[l1] != '\0')    // alternative implementation of strlen()
-        l1++;
-    while(substr[l2] != '\0') // alternative implementation of strlen()
-        l2++;
-    for (i = 0; i < l1;) {
-        j = 0;
-        count=0;
-        while ((str[i] == substr[j]) && (i != l1)) {  // compare each char of string and substring
-            count++;
-            i++;
-            j++;
+    int i,k,count;
+    i=0;
+    k=0;
+    count=0;
+    while(str[i]!='\0')
+    {
+        while(str[i+k]==substr[k] && substr[k]!='\0')
+        {
+            if(substr[k+1]=='\0')
+                count++;
+            k++;
         }
-        if (count == l2) {  // increment substring found counter if the same chars -
-            count1++;                // found are of the same length of substr
-            count = 0;          // reset counter back to 0
-        }
-        else                // if char doesnt match
-           i++;
+        i++;
+        k=0;
     }
-    return count1;
+    return count;
 }
